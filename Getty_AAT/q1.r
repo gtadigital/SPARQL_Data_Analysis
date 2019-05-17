74df71e70a2472527188c6b9ac194134061f2f96
matteoLorenzini <matteo.lorenzini@gmail.com>
2019-05-17 10:45:22 +0200

    Added chart and q1.r script

---
+++ Getty_AAT/q1.r
@@ -0,0 +1,28 @@
+
+library(SPARQL)
+library(ggplot2)
+library(reshape)
+library(scales)
+endpoint <- "http://vocab.getty.edu/sparql"
+
+#Q2 -   Number of triples per CLARIN graph
+
+q <- "SELECT ?facet ?label (count (?concept)as ?concept_NR) {
+  ?facet a gvp:Facet;
+           gvp:prefLabelGVP/xl:literalForm ?label;
+           skos:inScheme aat:.
+           
+  ?facet skos:member+ ?concept
+}
+
+GROUP BY ?facet ?label
+  "
+
+res <- SPARQL(url=endpoint, q)$results
+
+
+ggplot(res, aes(x=reorder(label, -concept_NR), 
+y=concept_NR, fill=label)) + theme_bw() + geom_bar(stat='identity') + 
+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) + 
+ggtitle('All the facets from AAT thesaurus with the number of the direct sub-concepts') + 
+labs(x='Facet'	, y='Nr of direct concepts') + theme(legend.position='none')
